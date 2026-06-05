e3c51118b16c7bb27418366628b934dacf4c20ab
Nigg Lukas (IT-DA-CEN5) <lukas_mario.nigg@sbb.ch>
2026-06-05 11:59:38 +0200

    Add login page

--- buenzli_frontend/src/app/app.ts
+++ buenzli_frontend/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginPage } from './shop/login-page/login-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginPage],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('buenzli_frontend');
+  protected readonly username = signal('');
+  protected readonly password = signal('');
+  protected readonly isAdmin = signal(false);
 }
