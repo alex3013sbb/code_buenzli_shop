346e88cc9b63d3558063561305415816459f3cdf
e570276 <lukas.pazicky@rail.ch>
2026-06-05 14:17:48 +0200

    added product-page structure & product-cards

--- buenzli_frontend/src/app/shop/main-page/product-page/product-card/product-card.ts
+++ buenzli_frontend/src/app/shop/main-page/product-page/product-card/product-card.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Product } from '../../../shared/product';
 
 @Component({
   selector: 'app-product-card',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './product-card.html',
   styleUrl: './product-card.scss',
 })
-export class ProductCard {}
+export class ProductCard {
+  readonly product = input.required<Product>();
+}
--- buenzli_frontend/src/app/shop/main-page/product-page/product-page.ts
+++ buenzli_frontend/src/app/shop/main-page/product-page/product-page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductCard } from './product-card/product-card';
+import { Product } from '../../shared/product';
 
 @Component({
   selector: 'app-product-page',
@@ -7,4 +8,67 @@ import { ProductCard } from './product-card/product-card';
   templateUrl: './product-page.html',
   styleUrl: './product-page.scss',
 })
-export class ProductPage {}
+export class ProductPage {
+  protected readonly products = signal<Product[]>([
+    {
+      id: 1,
+      name: 'Top Quality Krawatte',
+      price: 15.3,
+      category: 'Krawatte',
+    },
+    {
+      id: 2,
+      name: "Andi's Special Krawatte",
+      price: 19.95,
+      category: 'Krawatte',
+    },
+    {
+      id: 3,
+      name: 'Traditionelle Wollmütze',
+      price: 23.45,
+      category: 'Kopfbedeckung',
+    },
+    {
+      id: 4,
+      name: 'Alpkräuter-Sitzkissen',
+      price: 34.5,
+      category: 'Wohnen',
+    },
+    {
+      id: 5,
+      name: 'Gipfelstürmer Socken',
+      price: 18.9,
+      category: 'Socken',
+    },
+    {
+      id: 6,
+      name: "Andi's Hüttentee-Mischung",
+      price: 12.5,
+      category: 'Lebensmittel',
+    },
+    {
+      id: 7,
+      name: 'Gestrickter Stirnband-Klassiker',
+      price: 21.0,
+      category: 'Kopfbedeckung',
+    },
+    {
+      id: 8,
+      name: 'Käsehobel mit Arvenholzgriff',
+      price: 45.0,
+      category: 'Küchenhelfer',
+    },
+    {
+      id: 9,
+      name: 'Handgewebter Brotsack',
+      price: 16.8,
+      category: 'Küche',
+    },
+    {
+      id: 10,
+      name: 'Kuschelige Alphütten-Decke',
+      price: 89.0,
+      category: 'Wohnen',
+    },
+  ]);
+}
