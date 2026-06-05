19fd676217a1e43af633c492f208bd4cbf53aca0
Nigg Lukas (IT-DA-CEN5) <lukas_mario.nigg@sbb.ch>
2026-06-05 13:21:05 +0200

    Create basic login page & store data in signal

--- buenzli_frontend/src/app/app.ts
+++ buenzli_frontend/src/app/app.ts
@@ -9,8 +9,22 @@ import { LoginPage } from './shop/login-page/login-page';
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('buenzli_frontend');
   protected readonly username = signal('');
   protected readonly password = signal('');
   protected readonly isAdmin = signal(false);
+
+  setUsername(newUsername: string) {
+    this.username.update((u) => newUsername);
+    console.log('set new username', this.username());
+  }
+
+  setPassword(newPassword: string) {
+    this.password.update((p) => newPassword);
+    console.log('set new password ', this.password());
+  }
+
+  setIsAdmin(newIsAdmin: boolean) {
+    this.isAdmin.update((a) => newIsAdmin);
+    console.log('set isAdmin', this.isAdmin());
+  }
 }
--- buenzli_frontend/src/app/shop/login-page/login-page.ts
+++ buenzli_frontend/src/app/shop/login-page/login-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { LoginForm } from './login-form/login-form';
 
 @Component({
@@ -7,4 +7,24 @@ import { LoginForm } from './login-form/login-form';
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
-export class LoginPage {}
+export class LoginPage {
+  readonly username = input.required<string>();
+  readonly password = input.required<string>();
+  readonly isAdmin = input.required<boolean>();
+
+  newUsername = output<string>();
+  newPassword = output<string>();
+  newIsAdmin = output<boolean>();
+
+  setUsername(newUsername: string) {
+    this.newUsername.emit(newUsername);
+  }
+
+  setPassword(newPassword: string) {
+    this.newPassword.emit(newPassword);
+  }
+
+  setIsAdmin(newIsAdmin: boolean) {
+    this.newIsAdmin.emit(newIsAdmin);
+  }
+}
